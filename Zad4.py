1d28ec6ed053d3157a954065a9f7d18f3855014c
KarolSerkowski <33605416+KarolSerkowski@users.noreply.github.com>
2018-12-07 12:22:41 +0100

    zapis - ok, dodane komunikaty i przygotowanie pod puste etc

--- Zad4.py
+++ Zad4.py
@@ -9,35 +9,39 @@ from tkinter import messagebox as msb
 class Application:
     fileName = datetime.datetime.today().strftime('%Y-%m-%d')
     directory = ""
+    path = ""
 
     def __init__(self):
         self.window = tk.Tk()
         self.window.bind("<Button-1>", self.click_ppm_lpm)
         self.window.bind("<Button-3>", self.click_ppm_lpm)
+        textInfo =  "Witaj, \n kliknij w to okno aby wybrać lokalizację zapisu plików: \n-plik data****-**-**.txt\n "
+        self.displayInfo(textInfo)
 
+        self.window.mainloop()
+
+    def displayInfo(self, textInfo):
         text = tk.StringVar()
         label = tk.Label(self.window, textvariable=text, padx=100, pady=20)
         label.pack()
-        text.set(
-            "Witaj, \n kliknij w to okno aby wybrać lokalizację zapisu plików: \n-plik data****-**-**.txt\n ")
-
-        # "pliki:\n - puste.txt, "
-        # " jeśli utworzony plik nie zawiera żadnych danych,\n-krótkie.txt, jeśli utworzony plik ma jedną linię,\n-średnie.txt, jeśli utworzony plik ma najwyżej 10 linii,\n"
-        # "-długie.txt, jeśli utworzony plik ma więcej niż 10 linii
-        self.window.mainloop()
+        text.set(textInfo)
 
     def click_ppm_lpm (self, event):
 
         self.save_file()
 
         msb.showinfo("Info", "Plik został utworzony, za chwile nastąpi jego edycja w notatniku")
+        execute = "notepad.exe " + self.path
+        os.system(execute)
+        newTextInfo= " Zostały utworzone pliki:"
+        self.displayInfo(newTextInfo)
 
 
     def save_file(self):
 
         self.directory = fd.askdirectory()  # wywołanie okna dialogowego do wskazania ścieżki do folderu docelowego
         if self.directory:
-            msb.showinfo("Info", "Wybrano taki folder {folder} do zapisu plików:".format(folder=self.directory))
+            msb.showinfo("Info", "Wybrano taki folder {folder} do zapisu plików.".format(folder=self.directory))
             filename = str(self.getDateToFileName())
 
         if filename:
@@ -53,10 +57,10 @@ class Application:
 
     def createFilePath(self):
         path = os.path.join(self.directory, self.fileName + ".txt")
+        self.path = path
         print (path)
         return path
 
-        # execute = "notepad.exe " + path
 
 
 apl = Application()
